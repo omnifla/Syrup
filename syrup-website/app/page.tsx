728a49b9954cb4a84b86a161fd754729bc8dfc3b
Abdallah-Alwarawreh <Abooddevyt@outlook.com>
2025-01-02 20:57:49 +0300

    fix: deploy fix

--- syrup-website/app/page.tsx
+++ syrup-website/app/page.tsx
@@ -5,11 +5,11 @@ import { Header } from "@/components/header";
 import { Hero } from "@/components/hero";
 import { HowItWorks } from "@/components/how-it-works";
 import { Features } from "@/components/features";
-import { Testimonials } from "@/components/testimonials";
 import { FAQ } from "@/components/faq";
 import { Footer } from "@/components/footer";
 import { Roboto } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Testimonials } from "@/components/testimonials";
 
 const roboto = Roboto({
     weight: "400",
