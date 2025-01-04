df5bc93aaf8612cffc068b705a301787d8555d26
Ludovic Morin <LudovicMorin357@hotmail.com>
2025-01-04 14:27:51 -0500

    feat: enhance Header and Settings components; add CenteredScrollZone for improved UI and localization support

--- Extension-React/src/components/Header.tsx
+++ Extension-React/src/components/Header.tsx
@@ -3,12 +3,14 @@ import Dropdown from "./Dropdown";
 const Header: React.FC<{}> = () => {
 
     return (
-        <header className="flex p-4 items-center justify-between">
-            <div className="left flex gap-4 flex-rows">
-                <a href="https://joinsyrup.com" target="_blank"><img className="w-8 h-8 rounded-[5px]" src="/icons/Syrup.png" alt="" /></a>
-                <h1 className="text-2xl font-bold">Syrup</h1>
+        <header className="w-[100%] h-[100%] bg-background/90">
+            <div className="p-4 flex items-center gap-4 flex-rows">
+                <div className="flex justify-between">
+                    <a href="https://joinsyrup.com" target="_blank"><img className="w-5 h-5 rounded-[5px]" src="/icons/Syrup.png" alt="" /></a>
+                    <h1 className="text-xl font-bold">Syrup</h1>
+                </div>
+                <Dropdown />
             </div>
-            <Dropdown />
         </header>
     );
 }
--- Extension-React/src/components/Settings.tsx
+++ Extension-React/src/components/Settings.tsx
@@ -3,20 +3,21 @@ import Footer from "./Footer";
 import { X } from "lucide-react";
 import { LanguageSwitcher } from "@/components/LanguageSwitcher";
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { useTranslation } from "react-i18next";
 
 const Settings: React.FC<{}> = () => {
-
+    const { t } = useTranslation();
     return (
         <div className="settings w-[100%] h-[100%] grid grid-cols-1 grid-rows-[10%,80%,10%] fixed top-0 left-0 hidden bg-background">
             <header className="flex flex-rows justify-between w-full p-4">
-                <h1 className="text-2xl font-bold">Settings</h1>
+                <h1 className="text-2xl font-bold">{ t('Settings') }</h1>
                 <Button variant="ghost" size="icon" onClick={() => {
                         const settings = document.querySelector('.settings') as HTMLDivElement;
                         settings.classList.toggle('hidden');}}><X />
                 </Button>
             </header>
             <div className="center ">
-                <label htmlFor="theme-select" className="block text-sm font-medium">Theme</label>
+                <h1>{ t('Theme') }</h1>
                 <ThemeToggle />
                 <LanguageSwitcher />
             </div>
