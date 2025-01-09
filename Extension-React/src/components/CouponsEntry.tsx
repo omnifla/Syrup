fe54be56a9a93a10c9ef3b13fb889929058c82ca
Ludovic Morin <LudovicMorin357@hotmail.com>
2025-01-08 21:16:28 -0500

    Refactor CouponsEntry and CouponsPage components to use className prop and improve domain handling logic

--- Extension-React/src/components/CouponsEntry.tsx
+++ Extension-React/src/components/CouponsEntry.tsx
@@ -5,7 +5,7 @@ export interface CouponData {
     pageDomain: string;
     couponsDomain: Coupon[] | null;
     handleCopy: (code: string, index: number) => void;
-    isSubDomain: boolean;
+    className: string;
 }
 
 export interface Coupon {
@@ -16,18 +16,10 @@ export interface Coupon {
     score: number;
 }
 
-const CouponsEntry: React.FC<CouponData> = ({pageIcon, pageDomain, couponsDomain, handleCopy, isSubDomain}) => {
-
-    let height: string = ""
-
-    if (!isSubDomain) {
-        height = "h-[80vh]"
-    } else {
-        height = "h-[40vh]"
-    }
+const CouponsEntry: React.FC<CouponData> = ({pageIcon, pageDomain, couponsDomain, handleCopy, className}) => {
     
     return (
-        <div className={`${height} flex flex-col pt-2`}>
+        <div className={`flex flex-col pt-2 ${className}`}>
             <div className="flex items-center gap-4 pb-2 mb-2 pt-1 border-border border-b-2 border-t-2">
                 <img src={pageIcon} alt="Page Icon" className="w-8 h-z" />
                 <p className="text-lg font-bold text-primary">{pageDomain}</p>
--- Extension-React/src/components/CouponsPage.tsx
+++ Extension-React/src/components/CouponsPage.tsx
@@ -1,6 +1,7 @@
 import CouponsEntry from "@/components/CouponsEntry";
 import { useTranslation } from "react-i18next";
 import { Coupon } from "@/components/CouponCard";
+import { parseDomain, fromUrl } from "parse-domain";
 
 export interface CouponData {
     pageIcon: string;
@@ -16,32 +17,88 @@ export interface CouponData {
 const CouponsPage: React.FC<CouponData> = ({pageIcon, pageDomain, pageSubDomain, isSubDomain, couponsDomain, couponsSubDomain, handleCopy, errorMsg}) => {
     const { t } = useTranslation();
 
+    const parsedDomain = parseDomain(fromUrl(pageDomain));
+    const parsedSubDomain = parseDomain(fromUrl(pageSubDomain));
+
+    const getDomainName = (parsed: ReturnType<typeof parseDomain>) => {
+        if (parsed.type === 'LISTED' && parsed.icann) {
+            return parsed.icann.domain;
+        }
+        return '';
+    };
+    const getSubDomainName = (parsed: ReturnType<typeof parseDomain>) => {
+        if (parsed.type === 'LISTED' && parsed.icann && parsed.icann.subDomains) {
+            return parsed.icann.subDomains.join('.');
+        }
+        return '';
+    }
+
+    const domainName = getDomainName(parsedDomain);
+    const subDomainName: string = getSubDomainName(parsedSubDomain);
+
+    const handleDomain = () => {
+        const domain = document.querySelector('input[name="couponType"]:checked')?.id;
+        if (domain === 'domain') {
+            document.querySelector('.Domain')?.classList.remove('hidden');
+            document.querySelector('.SubDomain')?.classList.add('hidden');
+            document.querySelector('.DomainLabel')?.classList.add('bg-card');
+            document.querySelector('.SubDomainLabel')?.classList.remove('bg-card');
+        } else {
+            document.querySelector('.Domain')?.classList.add('hidden');
+            document.querySelector('.SubDomain')?.classList.remove('hidden');
+            document.querySelector('.SubDomainLabel')?.classList.add('bg-card');
+            document.querySelector('.DomainLabel')?.classList.remove('bg-card');
+        }
+    }
+
     return (
-        <div className="w-screen h-screen flex flex-col pt-2">
-                <h2 className="text-lg font-semibold pb-2 mb-2 text-primary text-center border-border border-b-2">
-                    { t('Coupons') }
-                </h2>
+        <div className="w-full h-full grid grid-rows-[12%,88%]">
+            <div className="domain-switch flex flex-row h-[90%] w-[90%] border-border border-2 rounded-lg mx-[5%]">
+                {errorMsg ? (
+                    <div className="w-[100%] m-2 flex items-center justify-center bg-card rounded-lg text-foreground text-sm">
+                        <p className="text-sm text-center text-red-500 dark:text-red-300">
+                            {t(errorMsg)}
+                        </p>
+                    </div>
+                ) : (
+                    <>
+                        {isSubDomain ? (
+                            <>
+                                <label htmlFor="domain" className="DomainLabel w-[50%] m-2 flex items-center justify-center rounded-lg text-foreground text-sm cursor-pointer">
+                                    {domainName}
+                                    <input className="hidden" type="radio" name="couponType" id="domain" onChange={handleDomain}/>
+                                </label>
+                                <label htmlFor="subDomain" className="SubDomainLabel w-[50%] m-2 flex items-center justify-center rounded-lg text-foreground text-sm cursor-pointer">
+                                    {subDomainName}
+                                    <input className="hidden" type="radio" name="couponType" id="subDomain" onChange={handleDomain}/>
+                                </label>
+                            </>
+                        ) : (
+                            <label htmlFor="domain" className="w-[100%] m-2 flex items-center justify-center bg-card rounded-lg text-foreground text-sm cursor-pointer">
+                                {domainName}
+                                <input className="hidden" type="radio" name="couponType" id="domain"/>
+                            </label>
+                        )}
+                    </>
+                )}
+            </div>
             {errorMsg ? (
-                <p className="text-sm text-center text-red-500 dark:text-red-300">
-                    {t(errorMsg)}
-                </p>
+                <div></div>
             ) : (
-                <>
+                <div>
                     <CouponsEntry
+                        className="h-[100%] Domain"
                         pageIcon={pageIcon}
                         pageDomain={pageDomain}
                         couponsDomain={couponsDomain}
-                        handleCopy={handleCopy}
-                        isSubDomain={isSubDomain} />
-                    {isSubDomain && (
-                        <CouponsEntry
-                            pageIcon={pageIcon}
-                            pageDomain={pageSubDomain}
-                            couponsDomain={couponsSubDomain}
-                            handleCopy={handleCopy}
-                            isSubDomain={isSubDomain} />
-                    )}
-                </>
+                        handleCopy={handleCopy} />
+                    <CouponsEntry
+                        className="h-[100%] SubDomain hidden"
+                        pageIcon={pageIcon}
+                        pageDomain={pageSubDomain}
+                        couponsDomain={couponsSubDomain}
+                        handleCopy={handleCopy} />
+                </div>
             )}
         </div>
     );
